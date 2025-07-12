ef9cb9abb4293b01ee417fe110d61facfbcee72d
Enyinnaya-x <ekevictor84@gmail.com>
2025-07-12 22:55:51 +0100

    forgot to push

--- src/components/Gamecard.vue
+++ src/components/Gamecard.vue
@@ -3,21 +3,28 @@
     import limbo from "../assets/images/limboimg.jpg"
     import dice from "../assets/images/diceimg.jpg";
     import { Gamepad2 } from 'lucide-vue-next';
-    const games = [
-     { image :  Slotsimage, name : "Slots"},
-    {  image : limbo, name : "Limbo"},
-     { image : dice, name : "Dice"}
-    ]
 </script>
 
 <template>
     <div class="kingholder w-4/5 mx-auto">
         <h2 class="font-bold text-bitPurple lg:text-3xl text-xl my-6">Featured games</h2>
         <div class=" flex flex-col gap-3 lg:flex-row items-center justify-center imgholder">
-            <div class="w-80 h-52 px-4 text-center" v-for="(game, index) in games" :key="index">
-                <img :src="game.image" alt="" class="w-full h-40 rounded-md object-cover">
+            <div class="w-80 h-52 px-4 text-center">
+                <img :src="Slotsimage" alt="" class="w-full h-40 rounded-md object-cover">
                 <div class="gamediv mt-3 flex justify-center">
-                    <button class="text-bitPurple font-semibold flex items-center px-3 py-1 text-lg  bg-white rounded-lg hover:bg-bitGold active:bg-bitGold">Play {{ game.name}} <Gamepad2 class="ms-2 text-bitPurple stroke-[2.5]"/></button>
+                    <button class="text-bitPurple font-semibold flex items-center px-3 py-1 text-lg  bg-white rounded-lg hover:bg-bitGold active:bg-bitGold">Play Slots <Gamepad2 class="ms-2 text-bitPurple stroke-[2.5]"/></button>
+                </div>
+            </div>
+            <div class="w-80 h-52 px-4 text-center">
+                <img :src="limbo" alt="" class="w-full h-40 rounded-md object-cover">
+                <div class="gamediv mt-3 flex justify-center">
+                    <router-link to="/limbo" class="text-bitPurple font-semibold flex items-center px-3 py-1 text-lg  bg-white rounded-lg hover:bg-bitGold active:bg-bitGold">Play Limbo <Gamepad2 class="ms-2 text-bitPurple stroke-[2.5]"/></router-link>
+                </div>
+            </div>
+            <div class="w-80 h-52 px-4 text-center">
+                <img :src="dice" alt="" class="w-full h-40 rounded-md object-cover">
+                <div class="gamediv mt-3 flex justify-center">
+                    <button class="text-bitPurple font-semibold flex items-center px-3 py-1 text-lg  bg-white rounded-lg hover:bg-bitGold active:bg-bitGold">Play Dice <Gamepad2 class="ms-2 text-bitPurple stroke-[2.5]"/></button>
                 </div>
             </div>
         </div>
--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Home from '../views/Home.vue'
 import Signup from '../views/Signup.vue'
 import Login from '../views/Login.vue'
 import Forgot from '../views/Forgot.vue'
+import limbo from '../views/limbo.vue'
 
 const routes = [
   { path: '/', name: 'Signup', component: Signup },
   { path: '/home', name: 'Home', component: Home },
   { path: '/login', name: 'Login', component: Login },
-  { path: '/forgot', name: 'Forgot', component: Forgot }
+  { path: '/forgot', name: 'Forgot', component: Forgot },
+  { path: '/limbo', name: 'limbo', component: limbo }
 ]
 
 const router = createRouter({
